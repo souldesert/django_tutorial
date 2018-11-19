20359f1b820ed002d15b8d2422545da461bc394d
Александр Васильков <alex@MacBook-Pro-Aleksandr.local>
2018-11-20 00:15:57 +0300

    Add question page

--- polls/views.py
+++ polls/views.py
@@ -21,7 +21,7 @@ def index(request):
 
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = { 
-        'latest_question_list': latest_question_list 
+        'latest_question_list': latest_question_list
     }
     return render(request, 'polls/index.html', context)
 
